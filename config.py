6e1ce8e25d2269bd790d6939f12706926db32cbe
Selfie SD <webtamil0@gmail.com>
2022-07-19 21:44:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 API_ID = int(os.environ.get("API_ID", 2791256))
 API_HASH = os.environ.get("API_HASH", "cd2fb4cdc795334aee3fbbc83da463ce")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5315089544:AAGJ1j4fmL8ZAD-WW8rqTBNJ8vbjpFJ_GwY")
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID","-1001720660922")
 IS_PRIVATE = os.environ.get("IS_PRIVATE", True) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID","1916694742"))
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1001576695301')
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1001599816148')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "1940030638").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
