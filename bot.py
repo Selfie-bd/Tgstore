4d53506af2f7f6d35ee18f897e05af0f1f78cdb2
Selfie SD <webtamil0@gmail.com>
2022-07-21 15:42:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,23 @@ def main():
                  workers=100)
 
     app.run()
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    if "|" in message.text:
+        link_parts = message.text.split("|")
+        link = link_parts[0]
+        aliases = link_parts[1:len(message.text) + 1]
+        alias1 = ""
+        for alias in aliases:
+            alias1 += alias
+        x = alias1.replace(" ", "")
+    else:
+        link = message.matches[0].group(0)
+        x= ""
+    short_link = await get_shortlink(link, x)
+    await message.reply(short_link, quote=True)
+
 async def get_shortlink(link, x):
     url = f'https://shorte.st/api'
     params = {'api': API_KEY,
