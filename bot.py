c33a1070cb748a7bd48248ccc76b245f3515f5aa
Selfie SD <webtamil0@gmail.com>
2022-07-21 15:39:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,21 @@ def main():
                  workers=100)
 
     app.run()
+async def get_shortlink(link, x):
+    url = f'https://shorte.st/api'
+    params = {'api': API_KEY,
+              'url': link,
+              'alias': x
+              }
 
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+            data = await response.json()
+            print(data["status"])
+            if data["status"] == "success":
+                return f"<code>{data['shortenedUrl']}</code>\n\nHere is your Link:\n{data['shortenedUrl']}"
+            else:
+                return f"Error: {data['message']}"
 
 if __name__ == "__main__":
     main()
